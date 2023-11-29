2281280da23bcbbe428bc6013f300892c0675b68
lue-bird <lue.the.bird@gmail.com>
2023-11-29 01:21:39 +0100

    Svg.element documentation +

--- src/BrowserApp/Svg.elm
+++ src/BrowserApp/Svg.elm
@@ -12,6 +12,9 @@ import BrowserApp exposing (DomNode)
 import BrowserApp.Dom exposing (Modifier)
 
 
+{-| Create an SVG element [`DomNode`](BrowserApp#DomNode).
+with a given tag, [`Modifier`](BrowserApp-Dom#Modifier)s and sub-nodes.
+-}
 element : String -> List (Modifier state) -> List (DomNode state) -> DomNode state
 element tag modifiers subs =
     BrowserApp.Dom.elementNamespaced "http://www.w3.org/2000/svg" tag modifiers subs
