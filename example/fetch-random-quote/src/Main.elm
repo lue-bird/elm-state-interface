19f1baac26afbb2115f045855fb7ddea6c595ec2
lue-bird <lue.the.bird@gmail.com>
2024-04-07 14:06:01 +0200

    example fetch-random-quote update and simplify

--- example/fetch-random-quote/src/Main.elm
+++ example/fetch-random-quote/src/Main.elm
@@ -3,11 +3,9 @@ port module Main exposing (main)
 import Color
 import Json.Decode
 import Json.Encode
-import Random.Pcg.Extended
 import Web
 import Web.Dom
 import Web.Http
-import Web.Random
 
 
 main : Web.Program State
@@ -27,18 +25,28 @@ port fromJs : (Json.Encode.Value -> event) -> Sub event
 
 type State
     = State
-        { movies : Maybe (Result Web.HttpError (Result { actualBody : String, jsonError : Json.Decode.Error } { content : String, author : String }))
+        { movies :
+            Maybe
+                (Result
+                    Web.HttpError
+                    (Result
+                        { actualBody : String, jsonError : Json.Decode.Error }
+                        { content : String, author : String }
+                    )
+                )
         }
 
 
 type DiceUiEvent
     = MoreClicked
-    | MoviesReceived (Result Web.HttpError (Result { actualBody : String, jsonError : Json.Decode.Error } { content : String, author : String }))
-
-
-codeRandomGenerator : Random.Pcg.Extended.Generator Int
-codeRandomGenerator =
-    Random.Pcg.Extended.int 100 500
+    | MoviesReceived
+        (Result
+            Web.HttpError
+            (Result
+                { actualBody : String, jsonError : Json.Decode.Error }
+                { content : String, author : String }
+            )
+        )
 
 
 initialState : State
@@ -48,95 +56,92 @@ initialState =
 
 interface : State -> Web.Interface State
 interface =
-    \state ->
-        case state of
-            State randomStuff ->
-                [ case randomStuff.movies of
-                    Just _ ->
-                        Web.interfaceNone
+    \(State state) ->
+        [ case state.movies of
+            Just _ ->
+                Web.interfaceNone
 
-                    Nothing ->
-                        Web.Http.get
-                            { url = "https://api.quotable.io/quotes/random"
-                            , headers = []
-                            , expect =
-                                Web.Http.expectJson
-                                    (Json.Decode.index 0
-                                        (Json.Decode.map2 (\content author -> { content = content, author = author })
-                                            (Json.Decode.field "content" Json.Decode.string)
-                                            (Json.Decode.field "author" Json.Decode.string)
-                                        )
-                                    )
-                            }
-                            |> Web.Http.request
-                            |> Web.interfaceFutureMap MoviesReceived
-                , Web.Dom.element "div"
-                    [ Web.Dom.style "background-color" (Color.rgb 0 0 0 |> Color.toCssString)
-                    , Web.Dom.style "color" (Color.rgb 1 1 1 |> Color.toCssString)
-                    , Web.Dom.style "font-size" "2em"
-                    , Web.Dom.style "padding-left" "80px"
-                    , Web.Dom.style "padding-right" "80px"
-                    , Web.Dom.style "position" "fixed"
-                    , Web.Dom.style "top" "0"
-                    , Web.Dom.style "right" "0"
-                    , Web.Dom.style "bottom" "0"
-                    , Web.Dom.style "left" "0"
-                    ]
-                    [ Web.Dom.element "div"
-                        [ Web.Dom.style "max-width" "870px"
-                        , Web.Dom.style "padding-top" "80px"
-                        ]
-                        [ Web.Dom.element "h1" [] [ Web.Dom.text "random quote" ]
-                        , Web.Dom.element "div"
-                            []
-                            [ Web.Dom.element "div"
-                                [ Web.Dom.style "font-size" "1.2em"
-                                ]
-                                [ case randomStuff.movies of
-                                    Nothing ->
-                                        Web.Dom.text "waiting for response"
-
-                                    Just (Err Web.HttpBadUrl) ->
-                                        Web.Dom.text "Malformed URL"
-
-                                    Just (Err Web.HttpNetworkError) ->
-                                        Web.Dom.text "Network error"
-
-                                    Just (Err (Web.HttpBadStatus _)) ->
-                                        Web.Dom.text "Bad response status"
-
-                                    Just (Ok (Err decodeError)) ->
-                                        Web.Dom.text
-                                            (decodeError.jsonError |> Json.Decode.errorToString)
-
-                                    Just (Ok (Ok movies)) ->
-                                        Web.Dom.element "div"
-                                            []
-                                            [ Web.Dom.element "blockquote" [ Web.Dom.style "textAlign" "center" ] [ Web.Dom.text movies.content ]
-                                            , Web.Dom.element "i" [ Web.Dom.style "textAlign" "center" ] [ Web.Dom.text ("by " ++ movies.author) ]
-                                            ]
-                                ]
-                            , Web.Dom.element "br" [] []
-                            , buttonUi
-                                [ Web.Dom.style "font-size" "2em"
-                                ]
-                                [ Web.Dom.text "next!" ]
-                                |> Web.Dom.futureMap (\() -> MoreClicked)
-                            ]
-                        ]
-                    ]
-                    |> Web.Dom.render
+            Nothing ->
+                Web.Http.get
+                    { url = "https://api.quotable.io/quotes/random"
+                    , expect =
+                        Web.Http.expectJson
+                            (Json.Decode.index 0
+                                (Json.Decode.map2 (\content author -> { content = content, author = author })
+                                    (Json.Decode.field "content" Json.Decode.string)
+                                    (Json.Decode.field "author" Json.Decode.string)
+                                )
+                            )
+                    }
+                    |> Web.Http.request
+                    |> Web.interfaceFutureMap MoviesReceived
+        , Web.Dom.element "div"
+            [ Web.Dom.style "background-color" (Color.rgb 0 0 0 |> Color.toCssString)
+            , Web.Dom.style "color" (Color.rgb 1 1 1 |> Color.toCssString)
+            , Web.Dom.style "font-size" "2em"
+            , Web.Dom.style "padding-left" "80px"
+            , Web.Dom.style "padding-right" "80px"
+            , Web.Dom.style "position" "fixed"
+            , Web.Dom.style "top" "0"
+            , Web.Dom.style "right" "0"
+            , Web.Dom.style "bottom" "0"
+            , Web.Dom.style "left" "0"
+            ]
+            [ Web.Dom.element "div"
+                [ Web.Dom.style "max-width" "870px"
+                , Web.Dom.style "padding-top" "80px"
                 ]
-                    |> Web.interfaceBatch
-                    |> Web.interfaceFutureMap
-                        (\event ->
-                            case event of
-                                MoreClicked ->
-                                    State { randomStuff | movies = Nothing }
+                [ Web.Dom.element "h1" [] [ Web.Dom.text "random quote" ]
+                , Web.Dom.element "div"
+                    []
+                    [ Web.Dom.element "div"
+                        [ Web.Dom.style "font-size" "1.2em"
+                        ]
+                        [ case state.movies of
+                            Nothing ->
+                                Web.Dom.text "waiting for response"
 
-                                MoviesReceived moviesResponse ->
-                                    State { randomStuff | movies = moviesResponse |> Just }
-                        )
+                            Just (Err Web.HttpBadUrl) ->
+                                Web.Dom.text "Malformed URL"
+
+                            Just (Err Web.HttpNetworkError) ->
+                                Web.Dom.text "Network error"
+
+                            Just (Err (Web.HttpBadStatus _)) ->
+                                Web.Dom.text "Bad response status"
+
+                            Just (Ok (Err decodeError)) ->
+                                Web.Dom.text
+                                    (decodeError.jsonError |> Json.Decode.errorToString)
+
+                            Just (Ok (Ok movies)) ->
+                                Web.Dom.element "div"
+                                    []
+                                    [ Web.Dom.element "blockquote" [ Web.Dom.style "textAlign" "center" ] [ Web.Dom.text movies.content ]
+                                    , Web.Dom.element "i" [ Web.Dom.style "textAlign" "center" ] [ Web.Dom.text ("by " ++ movies.author) ]
+                                    ]
+                        ]
+                    , Web.Dom.element "br" [] []
+                    , buttonUi
+                        [ Web.Dom.style "font-size" "2em"
+                        ]
+                        [ Web.Dom.text "next!" ]
+                        |> Web.Dom.futureMap (\() -> MoreClicked)
+                    ]
+                ]
+            ]
+            |> Web.Dom.render
+        ]
+            |> Web.interfaceBatch
+            |> Web.interfaceFutureMap
+                (\event ->
+                    case event of
+                        MoreClicked ->
+                            State { state | movies = Nothing }
+
+                        MoviesReceived moviesResponse ->
+                            State { state | movies = moviesResponse |> Just }
+                )
 
 
 buttonUi : List (Web.Dom.Modifier ()) -> List (Web.Dom.Node ()) -> Web.Dom.Node ()
